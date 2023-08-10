b651c3994458edee73cb5bf0f01e42e0c63e5050
Ähmed Bilal Siddiqui <ahmedbilal6880@gmail.com>
2023-08-10 18:20:26 +0500

    More LWC added

--- force-app/main/default/lwc/relatedRecordsList/relatedRecordsList.js
+++ force-app/main/default/lwc/relatedRecordsList/relatedRecordsList.js
@@ -1,16 +1,20 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 // import { getRecord } from 'lightning/uiRecordApi';
 import fetchRelatedRecordsList from '@salesforce/apex/fetchRelatedRecordsList.getRelatedContacts';
+import updateRecords from '@salesforce/apex/UpdateTable.updateRecords';
 import Contact from '@salesforce/schema/Contact.AccountId';
-
+// import { updateRecord } from 'lightning/uiRecordApi';
+// import { ShowToastEvent } from 'lightning/platformShowToastEvent'
+import { refreshApex } from '@salesforce/apex';
 const columns = [
-    {label:'Name',fieldName:'Name'},
-    {label:'Email',fieldName:'Email'},
+    {label:'Name',fieldName:'Name', editable : 'True'},
+    {label:'Email',fieldName:'Email', editable : 'True'},
 ];
 
 export default class RelatedRecordsDisplay extends LightningElement {
     @api recordId;
-    relatedContacts = [];
+    @track relatedContacts = [];
+    updatedContacts = [];
     columns = columns;
     @wire(fetchRelatedRecordsList, { accountId: '$recordId' })
     wiredContacts({error,data}){
@@ -22,5 +26,33 @@ export default class RelatedRecordsDisplay extends LightningElement {
     }
     get relatedRecords() {
         return this.Account.data.fields[Contact.AccountId].value;
+    }
+
+    handleSave(event){
+        console.log(event.detail.draftValues);
+        updateRecords({accountData:event.detail.draftValues}).then(result=>{
+            console.log(JSON.stringify(result));
+        }).catch(error=>{
+            console.log(JSON.stringify(error));
+        })
+        // this.updatedContacts = event.detail.draftValues;
+        // const inputItems = this.updatedContacts.slice().map(draft =>{
+        //     const fields = Object.assign({},draft);
+        //     return fields;
+        // });
+        // const promises = inputItems.map(recordInp =>updateRecord(recordInp));
+        // Promise.all(promises).then(res =>{
+        //     console.log('Response is: ' + res);
+        //     this.dispatchEvent(new ShowToastEvent({ title:'Success', message:'Record updated successfully', variant:'success' }));
+        //     this.updatedContacts = [];
+        //     return this.refresh();
+        // }).catch(error => {
+        //     console.log('Error is: ' + error);
+        //     this.dispatchEvent(new ShowToastEvent({ title:'Error', message:'Error occured while updating', variant:'error' }));
+        // })
+    }
+    
+    async refresh(){
+        await refreshApex(this.relatedContacts);
     }
 }
