59327db284696f1f7e624b1cdf02697080de5663
Ähmed Bilal Siddiqui <ahmedbilal6880@gmail.com>
2023-08-16 15:57:01 +0500

    Made table editable

--- force-app/main/default/lwc/relatedRecordsList/relatedRecordsList.js
+++ force-app/main/default/lwc/relatedRecordsList/relatedRecordsList.js
@@ -1,14 +1,14 @@
-import { LightningElement, api, wire, track } from 'lwc';
+import { LightningElement, wire, api, track } from 'lwc';
 // import { getRecord } from 'lightning/uiRecordApi';
 import fetchRelatedRecordsList from '@salesforce/apex/fetchRelatedRecordsList.getRelatedContacts';
-import updateRecords from '@salesforce/apex/UpdateTable.updateRecords';
-import Contact from '@salesforce/schema/Contact.AccountId';
-// import { updateRecord } from 'lightning/uiRecordApi';
-// import { ShowToastEvent } from 'lightning/platformShowToastEvent'
+import updateContacts from '@salesforce/apex/UpdateTabelContents.updateContacts';
+// import Contact from '@salesforce/schema/Contact.AccountId';
+// import { notifyRecordUpdateAvailable } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent'
 import { refreshApex } from '@salesforce/apex';
 const columns = [
-    {label:'Name',fieldName:'Name', editable : 'True'},
-    {label:'Email',fieldName:'Email', editable : 'True'},
+    {label:'Name',fieldName:'Name'},
+    {label:'Email',fieldName:'Email', editable : 'True'}
 ];
 
 export default class RelatedRecordsDisplay extends LightningElement {
@@ -19,40 +19,56 @@ export default class RelatedRecordsDisplay extends LightningElement {
     @wire(fetchRelatedRecordsList, { accountId: '$recordId' })
     wiredContacts({error,data}){
         if (data) {
+            console.log(data);
             this.relatedContacts = data;
         }else if (error){
-            // System.debug(error);
+            console.log(error);
         }
     }
-    get relatedRecords() {
-        return this.Account.data.fields[Contact.AccountId].value;
+    // get relatedRecords() {
+    //     return this.Account.data.fields[Contact.AccountId].value;
+    // }
+
+
+    async handleSave(event) {
+        const updatedFields = event.detail.draftValues;
+        // Prepare the record IDs for notifyRecordUpdateAvailable()
+        // const notifyChangeIds = updatedFields.map(row => { return { "recordId": row.Id } });
+        try {
+            // Pass edited fields to the updateContacts Apex controller
+            const result = await updateContacts({data: updatedFields});
+            console.log(JSON.stringify("Apex update result: "+ result));
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Contact updated',
+                    variant: 'success'
+                })
+            );
+             this.draftValues = [];
+             document.location.reload(true);
+            } catch(error) {
+               this.dispatchEvent(
+                   new ShowToastEvent({
+                       title: 'Error updating or refreshing records',
+                       message: error.body.message,
+                       variant: 'error'
+                    })
+             );}
     }
 
-    handleSave(event){
-        console.log(event.detail.draftValues);
-        updateRecords({accountData:event.detail.draftValues}).then(result=>{
-            console.log(JSON.stringify(result));
-        }).catch(error=>{
-            console.log(JSON.stringify(error));
-        })
-        // this.updatedContacts = event.detail.draftValues;
-        // const inputItems = this.updatedContacts.slice().map(draft =>{
-        //     const fields = Object.assign({},draft);
-        //     return fields;
-        // });
-        // const promises = inputItems.map(recordInp =>updateRecord(recordInp));
-        // Promise.all(promises).then(res =>{
-        //     console.log('Response is: ' + res);
-        //     this.dispatchEvent(new ShowToastEvent({ title:'Success', message:'Record updated successfully', variant:'success' }));
-        //     this.updatedContacts = [];
-        //     return this.refresh();
-        // }).catch(error => {
-        //     console.log('Error is: ' + error);
-        //     this.dispatchEvent(new ShowToastEvent({ title:'Error', message:'Error occured while updating', variant:'error' }));
-        // })
+    ShowToast(title, message, variant, mode){
+        const evt = new ShowToastEvent({
+                title: title,
+                message:message,
+                variant: variant,
+                mode: mode
+            });
+            this.dispatchEvent(evt);
     }
-    
-    async refresh(){
-        await refreshApex(this.relatedContacts);
+ 
+    // This function is used to refresh the table once data updated
+    async refresh() {
+        await refreshApex(this.wiredContacts);
     }
 }
